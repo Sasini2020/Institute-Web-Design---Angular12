87a84fd24730f8bde222c413220ac9a447ec72a3
ֆǟֆɨռɨ <63692107+Sasini2020@users.noreply.github.com>
2021-07-02 15:38:19 +0530

    Updated

--- Server/routes/api/attendance.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-
-router.get('/get-attendance', (req, res) => {
-    res.send('<h1>attendance</h1>');
-});
-
-module.exports = router;
--- Server/routes/api/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const user = require('./users');
-const attendance = require('./attendance');
-
-router.use('/user', user);
-router.use('/attendance', attendance);
-
-module.exports = router;
